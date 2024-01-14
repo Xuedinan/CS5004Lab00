e3facfb9b6480cc83e1aa8bcb2db378ced3d464e
DinanG <gaoxuedinan@gmail.com>
2024-01-13 18:44:00 -0800

    update extension practice files

---
+++ ExtensionPractice/Animal/Animal.java
@@ -0,0 +1,66 @@
+package Animal;
+
+/**
+ * Edited by: Xuedinan Gao
+ * Assignment: CS5004 Lab0
+ * Date: 01/13/2024
+ * 
+ * This class represents a Animal. A Animal has a name, an age and a skin color.
+ */
+
+public class Animal{
+	private String name;
+	private int age;
+	private String skinColor;
+
+  /**
+   * Construct a Animal object that has the provided name, age and  skin color.
+   *
+   * @param name  the name to be given to this animal
+   * @param age the age to be given to this animal
+   * @param price  the skin color to be given to this animal
+   */
+
+
+	public Animal(String name, int age, String skinColor) {
+		this.name = name;
+		this.age = age;
+		this.skinColor = skinColor;
+	}		
+	
+	public Animal() {
+	}
+	
+  /**
+   * Return the name of this animal.
+   *
+   * @return the name of this animal
+   */
+
+	public String getName() {
+		return name;
+	}
+	
+	
+  /**
+   * Return the age of this animal.
+   *
+   * @return the age of this animal
+   */
+	
+	public int getAge() {
+		return age;
+	}
+
+  /**
+   * Return the skin color of this animal.
+   *
+   * @return the skin color of this animal
+   */
+	
+	public String getSkinColor() {
+		return skinColor;
+	}
+
+	
+}
---
+++ testAnimal/test/AnimalTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Animal.Animal;
+
+/** 
+ * Edited by: Xuedinan Gao
+ * Assignment: CS5004 Lab0
+ * Date: 01/13/2024
+ * 
+ * A JUnit test class for the Book class.
+ * Create objective for book and person
+ * test if we could get correct information from Book class. 
+ * Book should have title, author, price.
+ * 
+ */
+
+public class AnimalTest {
+
+	Animal dog = new Animal("dog", 3, "white"); //create a animal with name dog, age 3 and skin color white
+	
+	@Test
+	public void testName() {
+		String expected = dog.getName(); // test if we could get animal name as "dog"
+		assertEquals(expected, "dog");
+	}
+
+	@Test
+	public void testAge() {
+		int expected = dog.getAge(); // test if we could get animal age as 3
+		assertEquals(expected, 3);
+	}
+
+	@Test
+	public void testSkinColor() {
+		String expected = dog.getSkinColor(); // test if we could get skin color name as "white"
+		assertEquals(expected, "white");
+	}
+
+}
